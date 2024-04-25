625fd841ef2d137a2ebe14df100894e3b3e23eaa
y.terui <sakulovesmileee@gmail.com>
2024-04-25 17:28:27 +0900

    トップページ　ハンバーガー

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-
+app.use("/assets", express.static("assets"))
 app.get("/", (req, res) => {
   res.render("index.ejs")
 })
