222978c2a1b7fdc59541ba15808c93281d8cac74
y.terui <sakulovesmileee@gmail.com>
2024-04-26 11:36:05 +0900

    編集・お気に入りページ

--- app.js
+++ app.js
@@ -21,4 +21,10 @@ app.get("/logout", (req, res) => {
 app.get("/all", (req, res) => {
   res.render("all.ejs")
 })
+app.get("/edit", (req, res) => {
+  res.render("edit.ejs")
+})
+app.get("/favorite", (req, res) => {
+  res.render("favorite.ejs")
+})
 app.listen(3000);
